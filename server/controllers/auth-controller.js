8ddb61f7b903780626fefb6209a8ac56332d43f9
Sylar009 <sanyamlawania@gmail.com>
2024-06-03 15:46:11 +0530

    Building a Complete User Login Route with Functionality in Our MERN App

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -23,26 +23,68 @@ const home = async (req, res) => {
 
 const register = async (req, res) => {
   try {
-    // console.log(req.body);
-
+    console.log(req.body);
     const { username, email, phone, password } = req.body;
 
-    const userExist = await User.findOne({ email: email });
+    const userExist = await User.findOne({ email });
+
     if (userExist) {
-      return res.status(400).send({ msg: "User already exists." });
+      return res.status(400).json({ message: "email already exists" });
     }
 
-    const userCreated = await User.create({ username, email, phone, password });
+    // hash the password
+    // const saltRound = 10;
+    // const hash_password = await bcrypt.hash(password, saltRound);
 
-    res.status(200).json({
-        msg: "Registeration Successful",
-        token: await userCreated.generateToken(),
-        userId: userCreated._id.toString(),
-      });
+    const userCreated = await User.create({
+      username,
+      email,
+      phone,
+      password,
+    });
 
+    res.status(201).json({
+      msg: "registration successful",
+      token: await userCreated.generateToken(),
+      userId: userCreated._id.toString(),
+    });
   } catch (error) {
-    res.status(400).send({ msg: "Page Not Found" });
+    // res.status(500).json("internal server error");
+    console.log(req.body);
+    next(error);
   }
 };
 
-module.exports = { home, register };
+// *----------------------------
+// User Login Logic
+// *----------------------------
+
+const login = async (req, res) => {
+    try {
+      const { email, password } = req.body;
+  
+      const userExist = await User.findOne({ email });
+      console.log(userExist);
+  
+      if (!userExist) {
+        return res.status(400).json({ message: "Invalid Credentials " });
+      }
+  
+      // const user = await bcrypt.compare(password, userExist.password);
+      const user = await userExist.comparePassword(password);
+  
+      if (user) {
+        res.status(200).json({
+          msg: "Login Successful",
+          token: await userExist.generateToken(),
+          userId: userExist._id.toString(),
+        });
+      } else {
+        res.status(401).json({ message: "Invalid email or password" });
+      }
+    } catch (error) {
+      res.status(500).json({msg: "Internal Server Error"});
+    }
+  };
+
+module.exports = { home, register, login };
