ca08dd34f44e53b54df84e106574e35684b02e25
Sylar009 <sanyamlawania@gmail.com>
2024-06-04 00:12:53 +0530

    Express error handling and validating login Schema

--- server/router/auth-router.js
+++ server/router/auth-router.js
@@ -17,12 +17,12 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth-controller");
-const {signupSchema} = require("../validators/auth-validator");
+const { signupSchema, loginSchema } = require("../validators/auth-validator");
 const validate = require("../middlewares/validate-middleware");
 
 
 router.route("/").get(authController.home);
 router.route("/register").post(validate(signupSchema), authController.register);
-router.route("/login").post(authController.login);
+router.route("/login").post(validate(loginSchema), authController.login);
 
 module.exports = router;
