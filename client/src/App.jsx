95599a2467d45fb678edf55ca9bee82d494d33f4
Sylar009 <sanyamlawania@gmail.com>
2024-06-04 11:28:32 +0530

    React Page Navigation with React Router DOM | MERN Series

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Contact } from "./pages/Contact";
+import { Service } from "./pages/Service";
+import { Register } from "./pages/Register";
+import { Login } from "./pages/Login";
+
 const App = () => {
-  return <h1>Hello Mern Series By Sanyam </h1>
+  // return <h1>Hello Mern Series By Sanyam </h1>
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/service" element={<Service />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </>
+  );
 };
 
 export default App;
