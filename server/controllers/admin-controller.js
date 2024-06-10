553356591ea03cd6985404c2de3a3cbc073fc9a0
Sanyam Lawania <39163842+Sylar009@users.noreply.github.com>
2024-06-10 17:15:57 +0530

    MERN Admin Panel Backend Fetching All The Contacts Data in Admin Dashboard

--- server/controllers/admin-controller.js
+++ server/controllers/admin-controller.js
@@ -1,4 +1,5 @@
 const User = require("../models/user-model");
+const Contact = require("../models/contact-model");
 
 // *-------------------------------
 //* getAllUsers Logic 📝
@@ -16,4 +17,25 @@ const getAllUsers = async (req, res) => {
   }
 };
 
-module.exports = getAllUsers;
+
+// *-------------------------------
+//* getAllContacts Logic 📝
+// *-------------------------------
+const getAllContacts = async (req, res) => {
+  try {
+    const contacts = await Contact.find();
+    console.log(contacts);
+    if (!contacts || contacts.length === 0) {
+      return res.status(404).json({ message: "No Contacts Found" });
+    }
+    return res.status(200).json(contacts);
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+module.exports = {
+  getAllUsers,
+  getAllContacts
+};
